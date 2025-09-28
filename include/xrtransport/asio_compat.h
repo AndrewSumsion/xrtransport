078f7f7869144137df39f163fbbc2c1ed42af833
Andrew Sumsion <andrew.sumsion7@gmail.com>
2025-09-27 20:25:43 -0600

    added async abstractions

--- include/xrtransport/asio_compat.h
+++ include/xrtransport/asio_compat.h
@@ -1,18 +1,32 @@
 #ifndef XRTRANSPORT_ASIO_COMPAT_H
 #define XRTRANSPORT_ASIO_COMPAT_H
 
+/*
+ * ASIO Compatibility Layer
+ *
+ * This file provides a compatibility layer between ASIO's template-based approach
+ * and an object-oriented approach. It defines abstract base classes that mirror
+ * ASIO's stream concepts (ReadableStream, WritableStream, etc.) while providing
+ * a virtual interface that can be implemented by concrete stream types.
+ *
+ * This allows for polymorphic use of different stream types (sockets, buffers,
+ * files, etc.) without requiring template instantiation at every call site.
+ */
+
 #define ASIO_STANDALONE
 #include "asio/buffer.hpp"
 #include "asio/error_code.hpp"
+#include "asio/socket_base.hpp"
 
 #include <cstddef>
+#include <functional>
 
 namespace xrtransport {
 
-// Base abstract class for non-blocking stream operations
-class NonBlockingStream {
+// Base abstract class for synchronous stream operations
+class SyncStream {
 public:
-    virtual ~NonBlockingStream() = default;
+    virtual ~SyncStream() = default;
 
     // Set or get blocking mode
     virtual void blocking_mode(bool mode) = 0;
@@ -22,10 +36,10 @@ public:
     virtual std::size_t available(asio::error_code& ec) = 0;
 };
 
-// Abstract class for read operations
-class ReadStream : virtual public NonBlockingStream {
+// Abstract class for synchronous read operations
+class SyncReadStream : virtual public SyncStream {
 public:
-    virtual ~ReadStream() = default;
+    virtual ~SyncReadStream() = default;
 
     // Read some data from the stream
     virtual std::size_t read_some(const asio::mutable_buffer& buffers) = 0;
@@ -43,10 +57,10 @@ public:
     }
 };
 
-// Abstract class for write operations
-class WriteStream : virtual public NonBlockingStream {
+// Abstract class for synchronous write operations
+class SyncWriteStream : virtual public SyncStream {
 public:
-    virtual ~WriteStream() = default;
+    virtual ~SyncWriteStream() = default;
 
     // Write some data to the stream
     virtual std::size_t write_some(const asio::const_buffer& buffers) = 0;
@@ -64,11 +78,74 @@ public:
     }
 };
 
-// Abstract class for read/write operations
-class ReadWriteStream : public ReadStream, public WriteStream {
+// Abstract class for synchronous read/write operations
+class SyncDuplexStream : public SyncReadStream, public SyncWriteStream {
 public:
-    virtual ~ReadWriteStream() = default;
+    virtual ~SyncDuplexStream() = default;
 };
+
+// Base abstract class for asynchronous stream operations
+class AsyncStream {
+public:
+    virtual ~AsyncStream() = default;
+
+    // ASIO-compatible async_wait for readiness notification
+    template<typename WaitHandler>
+    void async_wait(asio::socket_base::wait_type wait_type, WaitHandler&& handler) {
+        async_wait_impl(wait_type, std::forward<WaitHandler>(handler));
+    }
+
+protected:
+    virtual void async_wait_impl(asio::socket_base::wait_type wait_type, std::function<void(asio::error_code)> handler) = 0;
+};
+
+// Abstract class for asynchronous read operations
+class AsyncReadStream : virtual public AsyncStream {
+public:
+    virtual ~AsyncReadStream() = default;
+
+    // Async read some data from the stream
+    template<typename MutableBufferSequence, typename ReadHandler>
+    void async_read_some(const MutableBufferSequence& buffers, ReadHandler&& handler) {
+        async_read_some_impl(asio::mutable_buffer(buffers), std::forward<ReadHandler>(handler));
+    }
+
+protected:
+    virtual void async_read_some_impl(const asio::mutable_buffer& buffers, std::function<void(asio::error_code, std::size_t)> handler) = 0;
+};
+
+// Abstract class for asynchronous write operations
+class AsyncWriteStream : virtual public AsyncStream {
+public:
+    virtual ~AsyncWriteStream() = default;
+
+    // Async write some data to the stream
+    template<typename ConstBufferSequence, typename WriteHandler>
+    void async_write_some(const ConstBufferSequence& buffers, WriteHandler&& handler) {
+        async_write_some_impl(asio::const_buffer(buffers), std::forward<WriteHandler>(handler));
+    }
+
+protected:
+    virtual void async_write_some_impl(const asio::const_buffer& buffers, std::function<void(asio::error_code, std::size_t)> handler) = 0;
+};
+
+// Abstract class for asynchronous read/write operations
+class AsyncDuplexStream : public AsyncReadStream, public AsyncWriteStream {
+public:
+    virtual ~AsyncDuplexStream() = default;
+};
+
+// Abstract class for full duplex operations (both sync and async)
+class DuplexStream : public SyncDuplexStream, public AsyncDuplexStream {
+public:
+    virtual ~DuplexStream() = default;
+};
+
+// Legacy type aliases for backward compatibility
+using NonBlockingStream = SyncStream;
+using ReadStream = SyncReadStream;
+using WriteStream = SyncWriteStream;
+using ReadWriteStream = SyncDuplexStream;
 
 } // namespace xrtransport
 
